95b7cf414512159668faaca17799f3aaae03967b
williamferpc <willi_9212@hotmail.com>
2021-05-28 11:47:32 -0500

    cambios bootstrap 27-05-2021

---
+++ main.js
@@ -0,0 +1,24 @@
+// $('.carousel').carousel({
+//     interval: 1000,
+//     keyboard: true,
+//     ride: "true"
+// })
+
+// $(function () {
+//     let plantilla = `
+//     <div class="card" style="width: 18rem;">
+//         <img src="img/plumage176723_1920.jpg" class="card-img-top" alt="..." >
+//         <div class="card-body">
+//           <h5 class="card-title">Card title</h5>
+//           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+//           <a href="#" class="btn btn-primary">Go somewhere</a>
+//         </div>
+//     </div>
+//     `;
+//     $('#mostrarVentana').tooltip({
+//         title : " ",
+//         // animation : true,
+//         template : plantilla,
+//         sanitize: false
+//     })
+// })
